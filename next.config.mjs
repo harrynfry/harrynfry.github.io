e07a32d0c4817a2d05e12877595d0443e07a2e84
Harry Fry <harrynfry@gmail.com>
2024-07-22 17:27:44 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", 
   basePath: "/",
+  output: "export",
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
