95ec46ff1ed754cd80a39cd2e5b35a4a6bf4406e
Harry Fry <harrynfry@gmail.com>
2024-07-22 18:07:33 +0100

    ?

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-// .eslintrc.js
-module.exports = {
-  extends: ['next', 'next/core-web-vitals'],
-  rules: {
-    'import/no-anonymous-default-export': 'off',
-    'react/display-name': 'off',
-    // Add any additional rules or overrides here
-  },
-};
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   output: 'export', // For static export
   reactStrictMode: true,
   // Add any other configurations you need here
