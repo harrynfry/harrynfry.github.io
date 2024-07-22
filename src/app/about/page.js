3d4ea749dc808f2f0169b05082a1ba13dcf55858
Harry Fry <harrynfry@gmail.com>
2024-07-22 16:59:26 +0100

    about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -9,12 +9,12 @@ const AboutPage = () => {
             <h1 className="font-mono text-xl text-center">About me</h1>
             <p>
               After leaving school I became an apprentice chef, completing my qualifications at Bournemouth & Poole College. After a decade of experience in award winning restaurants,
-              I decided it was now or never to attempt to earn a degree. I graduated July 2024, attaining a First Class degree with Honours from Solent University.
+              I decided it was now or never to revisit my passion for tech and attempt to earn a degree. I graduated July 2024, attaining a First Class degree with Honours from Solent University.
             </p>
           </div>
           <div className="md:w-6/12 px-6 py-6 bg-white bg-opacity-40 flex items-center justify-center rounded-b-xl md:rounded-r-xl md:rounded-bl-none">
             <Image
-              className="rounded-xl aspect-square object-cover"
+              className="ring-8 ring-gray-400 ring-opacity-85 rounded-full aspect-square object-cover"
               src="/images/B6813DA4-A169-40B4-8718-0B4625531B16.JPG"
               alt="Image of Harry"
               width={200}
@@ -27,7 +27,7 @@ const AboutPage = () => {
         <div className="flex flex-col md:flex-row justify-center py-2 px-8">
           <div className="md:w-6/12 px-6 py-6 bg-white bg-opacity-40 flex items-center justify-center rounded-b-xl order-2 md:order-1 md:rounded-l-xl md:rounded-br-none md:rounded-tr-none">
             <Image
-              className="rounded-xl aspect-square object-cover"
+              className="ring-8 ring-slate-500 ring-opacity-70 rounded-full aspect-square object-cover"
               src="/images/a5be72c6-9cc9-4523-93cd-adb9a652d62b.JPG"
               alt="Image of Harry"
               width={200}
@@ -37,7 +37,9 @@ const AboutPage = () => {
           </div>
           <div className="md:w-6/12 px-6 py-6 bg-white bg-opacity-40 items-center justify-center rounded-t-xl order-1 md:order-2 md:rounded-r-xl md:rounded-tl-none font-mono">
             <h1 className="font-mono text-xl text-center">Interests</h1>
-            <p> hi
+            <p>
+              My main interests are in front-end development and user experience, I love seeing how client facing technologies can designed and applied, whilst being a part of researching design decisions!
+              My free time is usually spent being active, playing football, tennis or golf, or spending time with friends. 
             </p>
           </div>
         </div>
