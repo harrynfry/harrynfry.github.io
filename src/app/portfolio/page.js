5df6d44993e6cc397ef7ed68578a4fbf69979955
Harry Fry <harrynfry@gmail.com>
2024-07-31 17:28:21 +0100

    Update page.js

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -3,10 +3,11 @@ import TicTacToe from "../components/TicTacToe";
 const PortfolioPage = () => {
     return( 
         <div>
-
-        <p className="text-center text-xl">
-            Under construction... Check back to view my completed case studies and projects.
-        </p>
+            <div className="px-16 pt-6 font-mono">      
+                <p className="text-center text-xl">
+                    Under construction... Check back to view my completed case studies and projects.
+                </p>
+            </div>
 
         <div className="px-16 pt-6 font-mono">
             <p className="text-center text-xl">
