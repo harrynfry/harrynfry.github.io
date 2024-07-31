2a7608791c0608e40e41d902d6b6221c2b10eed6
Harry Fry <harrynfry@gmail.com>
2024-07-31 17:36:00 +0100

    Added diss

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -7,17 +7,24 @@ const PortfolioPage = () => {
                 <p className="text-center text-xl">
                     Under construction... Check back to view my completed case studies and projects.
                 </p>
+
+                <div className="px-16 pt-10 font-mono text-center underline hover:italic hover:font-bold hover:no-underline text-xl hover:text-shadow pb-8">
+                    <a href="/docs/Dissertation Report.pdf" target="_blank" rel="noopener noreferrer" download={"HarryFryDissertation"}>
+                    Download my Dissertation!
+                    </a>
+                </div>
+
                 <p className="text-center text-xl">
                     In the meantime, have a game of TicTacToe.
                 </p>
             </div>
 
-        <div className="px-16 pt-6 font-mono">
-            <p className="text-center text-xl">
-                Play a game        
-            </p>
-            <TicTacToe/>
-        </div>
+            <div className="px-16 pt-6 font-mono">
+                <p className="text-center text-xl">
+                    Play a game        
+                </p>
+                <TicTacToe/>
+            </div>
 
 
         </div>
